58613e179e8c2682a73b84ef8aed5b52e0867ad4
javierS04 <javiersemper509@gmail.com>
2024-07-15 15:17:15 +0100

    populated the board with pieces objects added symbols for each piece to make display easier

--- lib/board.rb
+++ lib/board.rb
@@ -43,63 +43,63 @@ class Board
   def populate_board
     #for white pieces
     #adding knights to the board
-    find_node([7, 1]).assign_piece("K")
-    find_node([7, 6]).assign_piece("K")
+    find_node([7, 1]).assign_piece(Knight.new([7,1], "white"))
+    find_node([7, 6]).assign_piece(Knight.new([7,6], "white"))
 
     #adding rooks to the board
-    find_node([0, 0]).assign_piece("R")
-    find_node([0, 7]).assign_piece("R")
+    find_node([7, 0]).assign_piece(Rook.new([7,0], "white"))
+    find_node([7, 7]).assign_piece(Rook.new([7,0], "white"))
 
     #adding pawns to the board
     (0...8).each do |index|
-      find_node([1, index]).assign_piece("P")
+      find_node([1, index]).assign_piece(Pawn.new([6,index], "white"))
     end
 
     #adding bishops to the board
-    find_node([7, 2]).assign_piece("B")
-    find_node([7, 5]).assign_piece("B")
+    find_node([7, 2]).assign_piece(Bishop.new([7,2], "white"))
+    find_node([7, 5]).assign_piece(Bishop.new([7,5], "white"))
 
     #adding queens to the board
-    find_node([7, 3]).assign_piece("Q")
+    find_node([7, 3]).assign_piece(Queen.new([7,3], "white"))
 
     #adding kings to the board
-    find_node([7, 4]).assign_piece("KN")
+    find_node([7, 4]).assign_piece(King.new([7,4], "white"))
 
 
     #for black pieces
     #adding knights to the board
-    find_node([0, 1]).assign_piece("K")
-    find_node([0, 6]).assign_piece("K")
+    find_node([0, 1]).assign_piece(Knight.new([0,1], "Black"))
+    find_node([0, 6]).assign_piece(Knight.new([0,6], "Black"))
 
     #adding rooks to the board
-    find_node([7, 0]).assign_piece("R")
-    find_node([7, 7]).assign_piece("R")
+    find_node([0, 0]).assign_piece(Rook.new([0,0], "Black"))
+    find_node([0, 7]).assign_piece(Rook.new([0,7], "Black"))
 
     #adding pawns to the board
     (0...8).each do |index|
-      find_node([6, index]).assign_piece("P")
+      find_node([6, index]).assign_piece(Pawn.new([1,index], "Black"))
     end
     #adding bishops to the board
-    find_node([0, 2]).assign_piece("B")
-    find_node([0, 5]).assign_piece("B")
+    find_node([0, 2]).assign_piece(Bishop.new([0,2], "Black"))
+    find_node([0, 5]).assign_piece(Bishop.new([0,5], "Black"))
 
     #adding queens to the board
-    find_node([0, 3]).assign_piece("Q")
+    find_node([0, 3]).assign_piece(Queen.new([0,3], "Black"))
 
     #adding kings to the board
-    find_node([0, 4]).assign_piece("KN")
+    find_node([0, 4]).assign_piece(King.new([0,4], "Black"))
   end
 
   def display_board
     populate_board
-    puts "  a b c d e f g h"
+    #puts "  a b c d e f g h"
     @board.each_with_index do |row, i|
       print "#{8 - i} "
       row.each do |cell|
-        piece = cell.piece_held.nil? ? "🩦" : cell.piece_held
+        piece = cell.piece_held.nil? ? "🩦" : cell.piece_held.sign
         print "#{piece} "
       end
-      puts "#{8 - i}"
+      puts
     end
     puts "  a b c d e f g h"
   end
--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,59 +1,107 @@
 class Pawn
   attr_accessor :possible_moves, :current_possition, :colour
 
-  def initialize (current, possible_moves = nil, colour)
+  def initialize (current, colour, possible_moves = nil)
     @current_possition = current
     @possible_moves = possible_moves
     @colour = colour
+  end
+
+  def sign
+    if @colour == "white"
+      return "♟︎"
+    else
+      return "♙"
+    end
   end
 end
 
 class Knight
   attr_accessor :possible_moves, :current_possition, :colour
 
-  def initialize (current, possible_moves = nil, colour)
+  def initialize (current, colour, possible_moves = nil)
     @current_possition = current
     @possible_moves = possible_moves
     @colour = colour
+  end
+
+  def sign
+    if @colour == "white"
+      return "♘"
+    else
+      return "♞"
+    end
   end
 end
 
 class Bishop
   attr_accessor :possible_moves, :current_possition, :colour
 
-  def initialize (current, possible_moves = nil, colour)
+  def initialize (current, colour, possible_moves = nil)
     @current_possition = current
     @possible_moves = possible_moves
     @colour = colour
+  end
+
+  def sign
+    if @colour == "white"
+      return "♗"
+    else
+      return "♝"
+    end
   end
 end
 
 class Rook
   attr_accessor :possible_moves, :current_possition, :colour
 
-  def initialize (current, possible_moves = nil, colour)
+  def initialize (current, colour, possible_moves = nil)
     @current_possition = current
     @possible_moves = possible_moves
     @colour = colour
+  end
+
+  def sign
+    if @colour == "white"
+      return "♖"
+    else
+      return "♜"
+    end
   end
 end
 
 class Queen
   attr_accessor :possible_moves, :current_possition, :colour
 
-  def initialize (current, possible_moves = nil, colour)
+  def initialize (current, colour, possible_moves = nil)
     @current_possition = current
     @possible_moves = possible_moves
     @colour = colour
+  end
+
+  def sign
+    if @colour == "white"
+      return "♕"
+    else
+      return "♛"
+    end
   end
 end
 
 class King
   attr_accessor :possible_moves, :current_possition, :colour
 
-  def initialize (current, possible_moves = nil, colour)
+  def initialize (current, colour, possible_moves = nil)
     @current_possition = current
     @possible_moves = possible_moves
     @colour = colour
+  end
+
+  def sign
+    if @colour == "white"
+      return "♔"
+    else
+      return "♚"
+    end
   end
 end
