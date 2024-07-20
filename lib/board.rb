96516f2a3ea200f06d5b6761b81d6b46e53d1ca1
javierS04 <javiersemper509@gmail.com>
2024-07-20 22:38:23 +0100

    Pieces are now able to take one another but cant take same coloured Pieces need to add alternating turns to test this properly

--- lib/board.rb
+++ lib/board.rb
@@ -16,7 +16,7 @@ end
 
 class Board
   attr_accessor :board
-
+  @@pieces_taken = []
   def initialize
     @board = create_board
   end
@@ -44,51 +44,51 @@ class Board
   def populate_board
     #for white pieces
     #adding knights to the board
-    find_node([7, 1]).assign_piece(Knight.new([7,1], "white"))
-    find_node([7, 6]).assign_piece(Knight.new([7,6], "white"))
+    find_node([7, 1]).assign_piece(Knight.new([7,1], "Black"))
+    find_node([7, 6]).assign_piece(Knight.new([7,6], "Black"))
 
     #adding rooks to the board
-    find_node([7, 0]).assign_piece(Rook.new([7,0], "white"))
-    find_node([7, 7]).assign_piece(Rook.new([7,0], "white"))
+    find_node([7, 0]).assign_piece(Rook.new([7,0], "Black"))
+    find_node([7, 7]).assign_piece(Rook.new([7,0], "Black"))
 
     #adding pawns to the board
     (0...8).each do |index|
-      find_node([6, index]).assign_piece(Pawn.new([6,index], "white"))
+      find_node([6, index]).assign_piece(Pawn.new([6,index], "Black"))
     end
 
     #adding bishops to the board
-    find_node([7, 2]).assign_piece(Bishop.new([7,2], "white"))
-    find_node([7, 5]).assign_piece(Bishop.new([7,5], "white"))
+    find_node([7, 2]).assign_piece(Bishop.new([7,2], "Black"))
+    find_node([7, 5]).assign_piece(Bishop.new([7,5], "Black"))
 
     #adding queens to the board
-    find_node([7, 3]).assign_piece(Queen.new([7,3], "white"))
+    find_node([7, 3]).assign_piece(Queen.new([7,3], "Black"))
 
     #adding kings to the board
-    find_node([7, 4]).assign_piece(King.new([7,4], "white"))
+    find_node([7, 4]).assign_piece(King.new([7,4], "Black"))
 
 
     #for black pieces
     #adding knights to the board
-    find_node([0, 1]).assign_piece(Knight.new([0,1], "Black"))
-    find_node([0, 6]).assign_piece(Knight.new([0,6], "Black"))
+    find_node([0, 1]).assign_piece(Knight.new([0,1], "white"))
+    find_node([0, 6]).assign_piece(Knight.new([0,6], "white"))
 
     #adding rooks to the board
-    find_node([0, 0]).assign_piece(Rook.new([0,0], "Black"))
-    find_node([0, 7]).assign_piece(Rook.new([0,7], "Black"))
+    find_node([0, 0]).assign_piece(Rook.new([0,0], "white"))
+    find_node([0, 7]).assign_piece(Rook.new([0,7], "white"))
 
     #adding pawns to the board
     (0...8).each do |index|
-      find_node([1, index]).assign_piece(Pawn.new([1,index], "Black"))
+      find_node([1, index]).assign_piece(Pawn.new([1,index], "white"))
     end
     #adding bishops to the board
-    find_node([0, 2]).assign_piece(Bishop.new([0,2], "Black"))
-    find_node([0, 5]).assign_piece(Bishop.new([0,5], "Black"))
+    find_node([0, 2]).assign_piece(Bishop.new([0,2], "white"))
+    find_node([0, 5]).assign_piece(Bishop.new([0,5], "white"))
 
     #adding queens to the board
-    find_node([0, 3]).assign_piece(Queen.new([0,3], "Black"))
+    find_node([0, 3]).assign_piece(Queen.new([0,3], "white"))
 
     #adding kings to the board
-    find_node([0, 4]).assign_piece(King.new([0,4], "Black"))
+    find_node([0, 4]).assign_piece(King.new([0,4], "white"))
   end
 
   def start_game
@@ -115,6 +115,11 @@ class Board
     piece = s.piece_held
 
     if s.piece_held && s.piece_held.possible_moves.include?(end_point) && legal_move(start_point, end_point, piece)
+      if e.piece_held != nil && e.piece_held.colour != s.piece_held.colour
+        piece_sign = e.piece_held.sign.gsub(/["]/, '')
+        @@pieces_taken << piece_sign
+      end
+
       if piece.class.name == "Pawn"
         e.assign_piece(piece.class.new(end_point, piece.colour, true))
       else
@@ -123,6 +128,8 @@ class Board
       @board[start_point[0]][start_point[1]] = Token.new([start_point[0], start_point[1]])
 
       display_board
+      p "Pieces taken so far"
+      puts "#{@@pieces_taken.join(' ')}"
     else
       p "Invalid move"
     end
@@ -135,15 +142,23 @@ class Board
     b = Board_knight.new(piece.move_off)
 
     moves = b.start_search(start_point, end_poinnt, piece.move_off)
-    x = moves.shift
+    x = moves.shift # first coordinate is the start so need to remove that from the array
+    x = moves.pop
     while !moves.empty?
       check = moves.shift
 
       if find_node(check).piece_held
-        p find_node(check).piece_held
         return nil
       end
     end
     return true
   end
 end
+
+
+# need to add alternating rounds so white then black then white and so fourth
+
+
+
+
+# need to add check and chekmate for the program
